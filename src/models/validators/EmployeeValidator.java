237a72526587dfcec659fca9c8d0c3a4d9a06a9b
nozomu.ueki <squall_nozomu.avance@outlook.jp>
2019-06-25 22:58:19 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -52,7 +52,7 @@ public class EmployeeValidator {
         return "";
     }
 
-    // 社員名の必須入力チェック
+  
     private static String _validateName(String name) {
         if(name == null || name.equals("")) {
             return "氏名を入力してください。";
@@ -61,9 +61,9 @@ public class EmployeeValidator {
         return "";
     }
 
-    // パスワードの必須入力チェック
+  
     private static String _validatePassword(String password, Boolean password_check_flag) {
-        // パスワードを変更する場合のみ実行
+        
         if(password_check_flag && (password == null || password.equals(""))) {
             return "パスワードを入力してください。";
         }
